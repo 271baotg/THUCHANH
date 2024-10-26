b14efcbf5d3736bb7a902a3de86228ff39912085
271baotg <21521862@gm.uit.edu.vn>
2024-10-27 01:21:25 +0700

    first-commit

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using THUCHANH.Repository;
+
+namespace THUCHANH.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductController(IProductRepository _productRepository)
+        {
+            productRepository = _productRepository;
+        }
+
+
+        [HttpGet]
+        public IActionResult getProducts()
+        {
+            var products = productRepository.getAll();
+            return Ok(products);
+        }
+    }
+}
---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using THUCHANH.Models;
+
+namespace THUCHANH.Repository
+{
+    public interface IProductRepository
+    {
+        public ICollection<Product> getById(int id);
+        public ICollection<Product> getAll();
+
+        public bool updateProduct(Product contact);
+        public bool createProduct(Product contact);
+
+        public bool isExist(long id);
+        public bool Save();
+    }
+}
